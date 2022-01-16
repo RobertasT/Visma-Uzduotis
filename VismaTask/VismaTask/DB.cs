4c70d59716ac25455922d358a13a4a093ddd2413
RobertasT <89936816+RobertasT@users.noreply.github.com>
2022-01-16 13:04:10 +0200

    Navigacija ir failo skaitymas

---
+++ VismaTask/VismaTask/DB.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace VismaTask
+{
+    public static class DB
+    {
+        const string FILE_NAME = "duomenys.json";
+
+        public static List<Meeting> Meetings = new List<Meeting>();
+
+        public static void SaveChanges()
+        {
+            var textData = JsonConvert.SerializeObject(Meetings);
+            File.WriteAllText(FILE_NAME, textData);
+        }
+
+        public static void Load()
+        {
+            if (File.Exists(FILE_NAME))
+            {
+                var textDataFromFile = File.ReadAllText(FILE_NAME);
+                var objectData = JsonConvert.DeserializeObject<List<Meeting>>(textDataFromFile);
+                Meetings = objectData;
+            }
+        }
+    }
+}
--- VismaTask/VismaTask/Program.cs
+++ VismaTask/VismaTask/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using VismaTask;
+
+bool exit = false;
+int selection = 0;
+
+DB.Load();
+
+while (!exit)
+{
+    Console.WriteLine("Pasirinkimai :");
+    Console.WriteLine("1 - Prideti nauja susitikima");
+    Console.WriteLine("2 - Istrinti susitikima");
+    Console.WriteLine("3 - Prideti zmogu prie susitikimo");
+    Console.WriteLine("4 - Isimti zmogu is susitikimo");
+    Console.WriteLine("5 - Perziureti visus susitikimus");
+    Console.WriteLine("6 - Baigti programa");
+
+    if (!int.TryParse(Console.ReadLine(), out selection) || selection>6 || selection<1)
+    {
+        Console.Clear();
+        Console.WriteLine("Netinkamai ivestas pasirinkimas");
+        continue;
+    }
+
+    switch (selection)
+    {
+        case 1: MeetingControler.Create();
+            break;
+        case 2:
+            MeetingControler.Delete();
+            break;
+        case 3:
+            MeetingControler.AddPerson();
+            break;
+        case 4:
+            MeetingControler.RemovePerson();
+            break;
+        case 5:
+            MeetingControler.GetAll();
+            break;
+        default: Console.Clear();
+            exit = true;
+            break;
+    }
+    
+}
